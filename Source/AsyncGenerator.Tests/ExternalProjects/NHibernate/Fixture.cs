265029aee04f6f9d1ebbb6588952522fda4dc7bd
maca88 <bostjan.markezic@siol.net>
2017-06-09 19:05:46 +0200

    Perfromance test simplified

--- Source/AsyncGenerator.Tests/ExternalProjects/NHibernate/Fixture.cs
+++ Source/AsyncGenerator.Tests/ExternalProjects/NHibernate/Fixture.cs
@@ -1,12 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
-using AsyncGenerator.Analyzation;
-using AsyncGenerator.Configuration;
+using System.Threading.Tasks;
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.FindSymbols;
+using Microsoft.CodeAnalysis.MSBuild;
 using NUnit.Framework;
 
 namespace AsyncGenerator.Tests.ExternalProjects.NHibernate
@@ -24,201 +27,70 @@ namespace AsyncGenerator.Tests.ExternalProjects.NHibernate
 	{
 		[Explicit]
 		[Test]
-		public void TestAfterTransformation()
+		public async Task PerformanceTest()
 		{
 			var slnFilePath = Path.GetFullPath(GetBaseDirectory() + @"..\..\ExternalProjects\NHibernate\Source\src\NHibernate.sln");
-			var config = AsyncCodeConfiguration.Create()
-				.ConfigureSolution(slnFilePath, s => s
-					//.RunInParallel()
-					.ConfigureProject("NHibernate", p => p
-						.ConfigureAnalyzation(a => a
-							.MethodConversion(GetMethodConversion)
-							.CallForwarding(true)
-							.CancellationTokens(t => t
-								.Guards(true)
-							)
-							.ScanMethodBody(true)
-						)
-						.ConfigureTransformation(t => t
-							.Disable()
-						)
-					)
-				);
-			var generator = new AsyncCodeGenerator();
-			Assert.DoesNotThrowAsync(async () => await generator.GenerateAsync(config));
+			var props = new Dictionary<string, string>
+			{
+				["CheckForSystemRuntimeDependency"] = "true"
+			};
+			var workspace = MSBuildWorkspace.Create(props);
+			var solution = await workspace.OpenSolutionAsync(slnFilePath).ConfigureAwait(false);
+			var project = solution.Projects.First(o => o.Name == "NHibernate");
+			var methodsList = await Task.WhenAll(project.Documents.Select(GetDocumentMethods));
+			var methods = methodsList.SelectMany(o => o).Select(o => o.OriginalDefinition).ToList();
+			var searchableProjects = new[] {project}.ToImmutableHashSet();
+
+			var interfaceMethods = methods.Where(o => o.ContainingType.TypeKind == TypeKind.Interface).ToList();
+			var abstractMethods = methods.Where(o => o.ContainingType.TypeKind != TypeKind.Interface && (o.IsAbstract || o.IsVirtual)).ToList();
+			var sw = new Stopwatch();
+
+			// Test FindImplementationsAsync performance
+			Console.WriteLine($"Start calling sequentially FindImplementationsAsync for {interfaceMethods.Count} interface methods");
+			sw.Start();
+			foreach (var interfaceMethod in interfaceMethods)
+			{
+				await SymbolFinder.FindImplementationsAsync(interfaceMethod, solution, searchableProjects);
+			}
+			Console.WriteLine($"Total elapsed time {sw.ElapsedMilliseconds}ms");
+
+			Console.WriteLine($"Start calling concurrently FindImplementationsAsync for {interfaceMethods.Count} interface methods");
+			sw.Restart();
+			foreach (var interfaceMethod in interfaceMethods)
+			{
+				await Task.WhenAll(SymbolFinder.FindImplementationsAsync(interfaceMethod, solution, searchableProjects));
+			}
+			Console.WriteLine($"Total elapsed time {sw.ElapsedMilliseconds}ms");
+
+
+			// Test FindImplementationsAsync performance
+			Console.WriteLine($"Start calling sequentially FindOverridesAsync for {abstractMethods.Count} abstract/virtual methods");
+			sw.Restart();
+			foreach (var abstractMethod in abstractMethods)
+			{
+				await SymbolFinder.FindOverridesAsync(abstractMethod, solution, searchableProjects);
+			}
+			Console.WriteLine($"Total elapsed time {sw.ElapsedMilliseconds}ms");
+
+			Console.WriteLine($"Start calling concurrently FindOverridesAsync for {abstractMethods.Count} abstract/virtual methods");
+			sw.Restart();
+			foreach (var abstractMethod in abstractMethods)
+			{
+				await Task.WhenAll(SymbolFinder.FindOverridesAsync(abstractMethod, solution, searchableProjects));
+			}
+			Console.WriteLine($"Total elapsed time {sw.ElapsedMilliseconds}ms");
+
 		}
 
-		private MethodConversion GetMethodConversion(IMethodSymbol symbol)
+		private async Task<List<IMethodSymbol>> GetDocumentMethods(Document document)
 		{
-			if (symbol.GetAttributes().Any(a => a.AttributeClass.Name == "ObsoleteAttribute"))
-			{
-				return MethodConversion.Ignore;
-			}
-			switch (symbol.ContainingType.Name)
-			{
-				case "HqlSqlWalker":
-					// The Seed method that was recognized to be async is never used as async.
-					// IsIntegral checks that the type is either short, int, or long; 
-					// the only type where SeedAsync is possible is DbTimestampType type and it fails into the "else if" branch. 
-					if (symbol.Name == "PostProcessInsert")
-					{
-						return MethodConversion.Ignore;
-					}
-					break;
-				case "SqlStatementLogger": //Generated because of Console.WriteLineAsync
-					return MethodConversion.Ignore;
-				case "IInterceptor": //We need to adjust proxy code generation to call IInterceptor async
-				case "IFieldInterceptor": //Is called by DefaultDynamicLazyFieldInterceptor.Intercept
-				case "AbstractFieldInterceptor":// The implementor of IFieldInterceptor
-					if (symbol.Name == "Intercept" || 
-						symbol.Name == "InitializeOrGetAssociation"//Private method of AbstractFieldInterceptor
-						) 
-					{
-						return MethodConversion.Ignore;
-					}
-					break;
-				case "ILazyPropertyInitializer": // Is called by AbstractFieldInterceptor.Intercept
-				case "AbstractEntityPersister":
-					if (symbol.Name == "InitializeLazyProperty" || 
-						symbol.Name == "InitializeLazyPropertiesFromDatastore" || // Private method from AbstractEntityPersister
-						symbol.Name == "InitializeLazyPropertiesFromCache") // Private method from AbstractEntityPersister
-					{
-						return MethodConversion.Ignore;
-					}
-					break;
-				case "BasicLazyInitializer": // Is called by DefaultLazyInitializer.Intercept
-					if (symbol.Name == "Invoke")
-					{
-						return MethodConversion.Ignore;
-					}
-					break;
-				case "SqlGenerator":
-				case "HqlLexer":
-					if (symbol.Name == "EvaluatePredicate")
-					{
-						// Generated because of Console.WriteLineAsync
-						return MethodConversion.Ignore;
-					}
-					break;
-				case "StatefulPersistenceContext":
-					if (
-						symbol.Name == "SetReadOnly" || // Generated because of proxy.HibernateLazyInitializer.GetImplementation, but it's not async in this case
-						symbol.Name == "Unproxy" // Generated because of proxy.HibernateLazyInitializer.GetImplementation, but it's not async in this case
-						)
-					{
-					
-						return MethodConversion.Ignore;
-					}
-					break;
-
-				case "CollectionType":
-					if (symbol.Name == "Contains")
-					{
-						// Generated because of proxy.HibernateLazyInitializer.GetImplementation, but it's not async in this case
-						return MethodConversion.Ignore;
-					}
-					break;
-
-				case "NHibernateProxyHelper":
-					if (symbol.Name == "GuessClass")
-					{
-						// Generated because of proxy.HibernateLazyInitializer.GetImplementation, but it's not async in this case
-						return MethodConversion.Ignore;
-					}
-					break;
-
-				case "NHibernateUtil":
-					if (symbol.Name == "IsPropertyInitialized")
-					{
-						// Generated because of proxy.HibernateLazyInitializer.GetImplementation, but it's not async in this case
-						return MethodConversion.Ignore;
-					}
-					break;
-				case "ISession":
-				case "ISessionImplementor":
-					if (symbol.Name == "BestGuessEntityName" || 
-						symbol.Name == "Contains")
-					{
-						// Generated because of proxy.HibernateLazyInitializer.GetImplementation, but it's not async in this case
-						return MethodConversion.Ignore;
-					}
-					break;
-
-				case "UnsavedValueFactory":
-					if (symbol.Name == "GetUnsavedVersionValue")
-					{
-						//The Seed method that was recognized to be async is never used as async.
-						return MethodConversion.Ignore;
-					}
-					break;
-
-				case "AbstractPersistentCollection":
-					if (symbol.Name == "ReadSize" ||
-					    symbol.Name == "ReadIndexExistence" ||
-					    symbol.Name == "ReadElementExistence" ||
-					    symbol.Name == "ReadElementByIndex" ||
-					    symbol.Name == "Read" || // Called by methods above
-					    symbol.Name == "Write"
-					)
-					{
-						//No one calls us :'(
-						return MethodConversion.Ignore;
-					}
-					break;
-
-				case "ICollectionPersister": // Called by AbstractPersistentCollection
-				case "AbstractCollectionPersister": // Implementation of ICollectionPersister
-					if (symbol.Name == "GetSize" ||
-					    symbol.Name == "IndexExists" ||
-					    symbol.Name == "ElementExists" ||
-					    symbol.Name == "GetElementByIndex" || 
-						symbol.Name == "Exists"
-					)
-					{
-						//No one calls us :'(
-						return MethodConversion.Ignore;
-					}
-					break;
-				case "IBatcher":
-					if (symbol.Name == "ExecuteReader" || symbol.Name == "ExecuteNonQuery")
-					{
-						return MethodConversion.ToAsync;
-					}
-					break;
-				default:
-					if (IsEventListener(symbol.ContainingType))
-					{
-						return MethodConversion.ToAsync;
-					}
-					break;
-			}
-			return MethodConversion.Unknown;
+			var rootNode = (CompilationUnitSyntax)await document.GetSyntaxRootAsync();
+			var semanticModel = await document.GetSemanticModelAsync();
+			return rootNode.DescendantNodes()
+				.OfType<MethodDeclarationSyntax>()
+				.Select(o => semanticModel.GetDeclaredSymbol(o))
+				.ToList();
 		}
 
-		private bool IsEventListener(INamedTypeSymbol type)
-		{
-			switch (type.Name)
-			{
-				case "IAutoFlushEventListener":
-				case "IFlushEventListener":
-				case "IDeleteEventListener":
-				case "ISaveOrUpdateEventListener":
-				case "IPostCollectionRecreateEventListener":
-				case "IPostCollectionRemoveEventListener":
-				case "IPostCollectionUpdateEventListener":
-				case "IPostDeleteEventListener":
-				case "IPostInsertEventListener":
-				case "IPostUpdateEventListener":
-				case "IPreCollectionRecreateEventListener":
-				case "IPreCollectionRemoveEventListener":
-				case "IPreCollectionUpdateEventListener":
-				case "IPreDeleteEventListener":
-				case "IPreInsertEventListener":
-				case "IPreLoadEventListener":
-				case "IPreUpdateEventListener":
-					return true;
-			}
-			return false;
-		}
 	}
 }
